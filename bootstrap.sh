4bcb3953fda2d7c8a4920745f4658094f4075bda
Aaron Lobo <lobo.aaron1@gmail.com>
2016-12-29 15:48:15 -0500

    Add support for aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Add Android SDK to path
+export PATH=$PATH:/Users/${USER}/Library/Android/sdk/platform-tools/
+
+# Add Java to path
+export PATH=$PATH:/Users/${USER}/Library/Java/Home
+
 # Get the dotfiles directory
 DOTFILES=$(dirname "${BASH_SOURCE}")
+
+source /$DOTFILES/aliases.sh
 
 # Source all functions
 for filename in /$DOTFILES/functions/*.*; do
