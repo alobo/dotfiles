cb7c1f4f3f3fb03f1cd2b1f7857408ebff2a3534
Aaron Lobo <lobo.aaron1@gmail.com>
2017-05-30 19:52:16 -0400

    Refactor paths to paths.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Add Android SDK to path
-export PATH=$PATH:/Users/${USER}/Library/Android/sdk/platform-tools/
-
-# Add Java to path
-export PATH=$PATH:/Users/${USER}/Library/Java/Home
-
 # Get the dotfiles directory
 export DOTFILES=$(dirname "${BASH_SOURCE}")
 
 source /$DOTFILES/aliases.sh
+
+source /$DOTFILES/paths.sh
 
 # Source all functions
 for filename in /$DOTFILES/functions/*.*; do
---
+++ paths.sh
@@ -0,0 +1,5 @@
+# Add Android SDK to path
+export PATH=$PATH:/Users/${USER}/Library/Android/sdk/platform-tools/
+
+# Add Java to path
+export PATH=$PATH:/Users/${USER}/Library/Java/Home
